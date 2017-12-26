c82cffbccab93bfa59a64d81e8a8166dbc6817af
ZSI2017 <13407121461@163.com>
2017-12-26 17:51:37 +0800

    add data-picker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,51 @@
 <template>
-<div class="hello">
-  <h1>{{ msg }}</h1>
-  <h2>Essential Links</h2>
-  <ul>
-    <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-    <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-    <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-    <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    <br>
-    <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-  </ul>
-  <h2>Ecosystem</h2>
-  <ul>
-    <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-    <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-    <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-    <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-  </ul>
-  <h2 @click ="toggleSeeDialog">点击打开弹窗-查看模式</h2>
-  <h2 @click ="toggleEditDialog">点击打开弹窗-编辑模式</h2>
-
-  <config-checkbox
-      :visible="dialogConfigVisible"
-      :sourceData="result"
-      :logisMerchId='logisMerchId'
-      :onlyRead='isCheckServer'
-      @listenToConfig="changeConfigVisible"
-    > </config-checkbox>
+<div class="block">
+  <span class="demonstration">默认</span>
+  <el-date-picker v-model="value3" @change="handleChange" type="datetimerange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
+  </el-date-picker>
 </div>
 </template>
 
 <script>
-import configCheckbox from "./configCheckbox";
 export default {
-  name: 'HelloWorld',
-  components:{
-    configCheckbox
-  },
   data() {
     return {
-     dialogConfigVisible:false,
-     result:{},
-     logisMerchId:1,
-     isCheckServer:true,
-      msg: 'Welcome to Your Vue.js App'
-    }
+      pickerOptions2: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近一个月',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近三个月',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            picker.$emit('pick', [start, end]);
+          }
+        }]
+      },
+      value3: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
+      value4: ''
+    };
   },
-  mounted() {
-    console.log(this.$http)
-    var _this = this;
-    this.$http.get('/list1', function(data) {
-      _this.result = data.data;
+  methods: {
+    handleChange(value) {
+      console.log(value);
 
-    })
-  },
-  methods:{
-    changeConfigVisible(flag){
-        this.dialogConfigVisible = flag;
-    },
-    toggleSeeDialog(){
-      this.isCheckServer = true;
-       this.dialogConfigVisible = !this.dialogConfigVisible;
-    },
-    toggleEditDialog(){
-      this.isCheckServer = false;
-       this.dialogConfigVisible = !this.dialogConfigVisible;
     }
-
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
